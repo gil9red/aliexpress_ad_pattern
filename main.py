1900c0f7a6c41e13287a67ac905e51de6c607ba8
Ilya Petrash <ip1992@inbox.ru>
2015-09-23 02:41:50 +0500

    Больше "Вася"

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ if __name__ == '__main__':
     NAME = 'Вася'
 
     font = ImageFont.truetype("arial.ttf", 26)
+    big_font = ImageFont.truetype("arial.ttf", 65)
 
     im = Image.open('aliexpress_ad.jpg')
 
     draw = ImageDraw.Draw(im)
-    draw.text((33, 384), NAME, font=font, fill="black")
-    draw.text((66, 455), NAME, font=font, fill="black")
-    draw.text((431, 504), NAME, font=font, fill="white")
+    draw.text((33, 384), NAME.title(), font=font, fill="black")
+    draw.text((66, 455), NAME.upper(), font=font, fill="black")
+    draw.text((431, 504), NAME.upper(), font=font, fill="white")
+    draw.text((52, 637), NAME.upper(), font=big_font, fill="white")
 
     im.save("runaway.jpg")
